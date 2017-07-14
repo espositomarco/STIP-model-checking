6d7a951f6fbc075779aa24da8f32829c0f8b1d40
giacomo <nazzarogiacomo@gmail.com>
2017-07-14 06:02:52 +0200

    Upgraded preprocess script. Now modules can be parametric in number of arguments.

--- preprocess.py
+++ preprocess.py
@@ -21,32 +21,67 @@ def process_body_for(command: str, text : str) -> str :
         result += text.replace(varname, str(i))
 
     message = '-- Code generated as <for> (preprocess.py)'
-    return message + result
+    return message, result
 
 
 def process_body_insert_string(insert_name: str) -> str:
     insert_string = eval(insert_name.strip())
     message = '-- Code generated as <insert_string> (preprocess.py)\n'
-    return message + insert_string
+    return message, insert_string
 
 
 def process_body_eval(body: str) -> str:
     eval_body = eval(body.strip())
     message = '-- Code generated as <eval_python> (preprocess.py)\n'
-    return message + str(eval_body)
+    return message, str(eval_body)
 
+def process_body_exec(body: str) -> str:
+    exec_body = exec(body.strip())
+    message = '-- Code generated as <exec_python> (preprocess.py)\n'
+    return message, ''
+
+def process_body_define(body: str) -> str:
+    var, val = body.strip().split('=')
+    exec_body = exec('global ' + var + ';' + body.strip())
+    message = '-- Code generated as <exec_python> (preprocess.py)\n'
+    return message, '' #var + ' := ' + val + ';'
+
+
+def process_body_list(command, body: str) -> str:
+    for_str, varname, in_str, range_str = command.split(' ')
+    assert(in_str == 'in')
+    result = ''
+    # print('cmon0', text)
+    lines = body.splitlines()
+    for i in range_string(range_str):
+        # for line in lines:
+            # print(line)
+        result += body.strip().replace(varname, str(i)) + ', '
+
+    message = '-- Code generated as <for> (preprocess.py)'
+    return message, result[:-2]
 
 def process_body(command : str, body : str) -> str :
     if command.startswith('for'):
         return process_body_for(command, body)
+    if command.startswith('list'):
+        return process_body_list(command, body)
     if command == 'insert_string':
         return process_body_insert_string(body)
-    if command == 'eval_python':
+    if command == 'eval':
         return process_body_eval(body)
-
-    print('ERROR in process_body\n','commmand:', command, 'body:', body)
-    assert(0)
-    return 'PREPROCESS_FAILURE'
+    if command == '':
+        return process_body_eval(body)
+    if command == 'exec':
+        return process_body_exec(body)
+    if command == 'define':
+        return process_body_define(body)
+    try:
+        return eval(command)
+    except:
+        print('ERROR in process_body\n','commmand:', command, 'body:', body)
+        assert(0)
+        return 'PREPROCESS_FAILURE'
 
 
 def find_closing_token(text: str, start: int, opening : str = '{', closing : str = '}') -> int:
@@ -90,6 +125,7 @@ def preprocess(text : str) -> str :
     preprocess_start = text.find(preprocess_token)
     while preprocess_start > 0:
         start = text.find(start_token, preprocess_start)
+   
         # end = text.find(end_token, start)
         end = find_closing_token(text, start, start_token, end_token)
 
@@ -99,7 +135,7 @@ def preprocess(text : str) -> str :
         command = text[preprocess_start+1: start].strip()
         body = text[start+len(start_token):end]
 
-        body_processed = process_body(command, body.rstrip() )
+        message, body_processed = process_body(command, body.rstrip() )
         text = text[:preprocess_start] + body_processed + text[end+len(end_token):]
         preprocess_start = text.find(preprocess_token, preprocess_start+len(start_token)+1)
 
